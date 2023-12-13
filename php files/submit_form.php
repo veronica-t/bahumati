1aafe8d4ad6cc6d3a82536b38d9b4984cd6de388
Reena Tanukula <tanukula@email.sc.edu>
2023-12-13 12:03:29 -0500

    latest edits

--- php files/submit_form.php
+++ php files/submit_form.php
@@ -1,20 +1,61 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dataForm');
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    //display the submitted data
-    echo "<h2>Form Data Submitted:</h2>";
-    echo "<p>Name: $name</p>";
-    echo "<p>Email: $email</p>";
-    echo "<p>Message: $message</p>";
-} else {
-    // redirect to the form page
-    header("Location: contact.html");
-    exit;
-}
+if($conn->connect_error) {
+    die('Connection Failed ' . $conn->connect_error);
+    }
 
-$conn->close();
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+        $sub = $_POST["submit"];
+        $query = $conn->prepare("INSERT INTO user_inquiries (Name,Email,Message,)
+        VALUES (?,?,?)");
+        $query->bind_param('ssss', $name, $email, $message, $sub);
+        $query->execute();
+        $conn->close();
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form</title>
+        <meta name="author" content="Veronica Tanukula">
+
+        
+        <script>  </script>
+    </head>
+    </body>
+
+        <form id="formal" method="POST">
+        <!-- Name Input -->
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Full Name Required *" required>
+
+        <br>
+
+        <!-- Email Input -->
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Required *" required>
+
+        <br>
+
+        <label for="message"></label>
+        <textarea id="message" name="message" rows="6" cols="40" placeholder="Let us know!" required></textarea>
+
+        <br>
+
+        <!-- Submit Button -->
+        <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
